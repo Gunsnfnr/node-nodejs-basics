4d9b7ef46bb4dfa4cd3f43506af90a73e0043c3e
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-03 22:58:49 +0300

    feat: add fs-list task solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir, access } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await access(path.join(__dirname, "/files"));
+
+    const files = await readdir(path.join(__dirname, "/files"));
+    for (const file of files) console.log(file);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
