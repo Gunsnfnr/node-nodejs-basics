6482ff3e6d8cb2e93f223ebfc3614a9c438d4b96
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-03 23:02:52 +0300

    feat: add fs-read task solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { access, readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await access(path.join(__dirname, "/files/fileToRead.txt"));
+
+    const contents = await readFile(
+      path.join(__dirname, "/files/fileToRead.txt"),
+      {
+        encoding: "utf8",
+      }
+    );
+    console.log(contents);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
