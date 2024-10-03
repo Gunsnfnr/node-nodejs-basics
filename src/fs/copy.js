72aaa295f8c0a6974248f4718a22664048a77928
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-03 22:33:22 +0300

    feat: add fs-copy task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import * as fsPromises from "node:fs/promises";
+import { access } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await access(path.join(__dirname, "/files"));
+
+    fsPromises
+      .cp(
+        path.join(__dirname, "/files"),
+        path.join(__dirname, "/files_copy "),
+        { recursive: true, force: false, errorOnExist: true },
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+        }
+      )
+      .catch(() => {
+        throw new Error("FS operation failed");
+      });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -6,18 +6,22 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+export function throwFSFailureError() {
+  throw new Error("FS operation failed");
+}
+
 const create = async () => {
   try {
-    await access(path.join(__dirname, "fresh.txt"));
+    await access(path.join(__dirname, "files/fresh.txt"));
     fsPromises
-      .readFile(path.join(__dirname, "fresh.txt"))
+      .readFile(path.join(__dirname, "files/fresh.txt"))
       .then((data) => {
         throw new Error("FS operation failed");
       })
       .catch(err);
   } catch (err) {
     const promise = fsPromises.writeFile(
-      path.join(__dirname, "fresh.txt"),
+      path.join(__dirname, "files/fresh.txt"),
       "I am fresh and young"
     );
     await promise;
