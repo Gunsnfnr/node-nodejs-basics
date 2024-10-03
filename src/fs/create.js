731055479b651760cde0d6f53401d7cf7a7079ed
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-03 20:22:07 +0300

    feat: add fs-create task solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import * as fsPromises from "node:fs/promises";
+import { access } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access(path.join(__dirname, "fresh.txt"));
+    fsPromises
+      .readFile(path.join(__dirname, "fresh.txt"))
+      .then((data) => {
+        throw new Error("FS operation failed");
+      })
+      .catch(err);
+  } catch (err) {
+    const promise = fsPromises.writeFile(
+      path.join(__dirname, "fresh.txt"),
+      "I am fresh and young"
+    );
+    await promise;
+  }
 };
 
 await create();
