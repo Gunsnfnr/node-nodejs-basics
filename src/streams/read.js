3a12ea3efcffc795bbe92832fbee3932911c9892
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-06 15:09:38 +0300

    feat: add stream:read task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import * as fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import * as os from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(
+    path.join(__dirname, "files/fileToRead.txt")
+  );
+
+  readStream.on("data", (dataChunk) => {
+    process.stdout.write(`${dataChunk}${os.EOL}`);
+  });
 };
 
 await read();
