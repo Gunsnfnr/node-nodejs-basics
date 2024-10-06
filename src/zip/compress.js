cb1b9f3bbfb411cb41e262f0cc09f0ceb0334028
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-06 18:55:39 +0300

    feat: add zip:compress task solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { pipeline } from "node:stream/promises";
+import * as fs from "node:fs";
+import * as zlib from "node:zlib";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(
+    fs.createReadStream(path.join(__dirname, "files", "fileToCompress.txt")),
+    zlib.createGzip(),
+    fs.createWriteStream(path.join(__dirname, "files", "archive.gz"))
+  );
+  console.log("Zipped successfully.");
 };
 
 await compress();
